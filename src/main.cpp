a15797ed1939caf1948feb20e194918e6497d5e2
tzshin <kabonplay@gmail.com>
2024-03-18 01:01:21 +0800

    MILESTONE! Everything seems fine. Added the pinout figure. Gyro calibrated.

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,28 @@ Everyone that sends me pictures and videos of your flying creations! -Nick
 
 //========================================================================================================================//
 
+// Pinout for TZ's FC v1
+// With Teensy 4.0
+// 
+//  SD_SCK  13 +---------+ 12  SD_MISO
+//    VBAT  14 |         | 11  SD_MOSI
+//     CUR  15 |         | 10  #SVO_WNG
+// IMU_SCL  16 |         | 09  #SVO_FPV
+// IMU_SDA  17 |         | 08  
+//          18 |         | 07  
+//          19 |         | 06  #SVO_PAN
+//   RX_TX  20 |         | 05  #SVO_TLT
+//   RX_RX  21 |         | 04  M3
+//      M2  22 |         | 03  M4
+//      M1  23 |         | 02  BEPR
+//         3v3 |  +---+  | 01  
+//         GND |  |   |  | 00  
+//         Vin +--+---+--+ GND 
+//
+// "#"s are project specific pin config
+
+//========================================================================================================================//
+
 #include <Arduino.h>
 
 //========================================================================================================================//
@@ -37,20 +59,19 @@ Everyone that sends me pictures and videos of your flying creations! -Nick
 // #define USE_SBUS_RX
 // #define USE_DSM_RX
 #define USE_CRSF_RX
-static const uint8_t num_DSM_channels = 6; // If using DSM RX, change this to match the number of transmitter channels you have
 
 // Uncomment only one IMU
 #define USE_MPU6050_I2C // Default
 // #define USE_MPU9250_SPI
 
 // Uncomment only one full scale gyro range (deg/sec)
-// #define GYRO_250DPS //Default
+// #define GYRO_250DPS // Default
 #define GYRO_500DPS
 // #define GYRO_1000DPS
 // #define GYRO_2000DPS
 
 // Uncomment only one full scale accelerometer range (G's)
-// #define ACCEL_2G //Default
+// #define ACCEL_2G // Default
 #define ACCEL_4G
 // #define ACCEL_8G
 // #define ACCEL_16G
@@ -59,14 +80,15 @@ static const uint8_t num_DSM_channels = 6; // If using DSM RX, change this to ma
 
 // REQUIRED LIBRARIES (included with download in main sketch folder)
 
-#include <Wire.h>     //I2c communication
-#include <SPI.h>      //SPI communication
-#include <PWMServo.h> //Commanding any extra actuators, installed with teensyduino installer
+#include <Wire.h>     // I2c communication
+#include <SPI.h>      // SPI communication
+#include <PWMServo.h> // Commanding any extra actuators, installed with teensyduino installer
 
 #if defined USE_SBUS_RX
-#include "SBUS.h" //sBus interface
+#include "SBUS.h" // sBus interface
 #elif defined USE_DSM_RX
 #include "DSMRX.h"
+static const uint8_t num_DSM_channels = 6; // If using DSM RX, change this to match the number of transmitter channels you have
 #elif defined USE_CRSF_RX
 #include "CRSFforArduino.hpp"
 CRSFforArduino *crsf = nullptr;
@@ -144,11 +166,19 @@ unsigned long channel_2_fs = 1500; // ail
 unsigned long channel_3_fs = 1500; // elev
 unsigned long channel_4_fs = 1500; // rudd
 unsigned long channel_5_fs = 1000; // gear, smaller than 1500 = throttle cut (modified to comply with ELRS convention)
-unsigned long channel_6_fs = 2000; // aux1
+unsigned long channel_6_fs = 2000; // aux1 (flying mode, 1: Quad, 2: Mixed, 3: Wing)
 #if defined USE_CRSF_RX
-unsigned long channel_7_fs = 2000; // aux2
-unsigned long channel_8_fs = 2000; // aux3
+unsigned long channel_7_fs = 1500; // aux2 (observer gimbal pan-axis)
+unsigned long channel_8_fs = 1500; // aux3 (observer gimbal tilt-axis)
 #endif
+
+const int servo1Default = 90; // Default servo position (deg)
+const int servo2Default = 90; // TODO: Determine the default poses
+const int servo3Default = 90;
+const int servo4Default = 90;
+// const int servo5Default = 90;
+// const int servo6Default = 90;
+// const int servo7Default = 90;
 
 // Filter parameters - Defaults tuned for 2kHz loop rate; Do not touch unless you know what you are doing:
 float B_madgwick = 0.04; // Madgwick filter parameter
@@ -166,11 +196,11 @@ float MagScaleZ = 1.0;
 
 // IMU calibration parameters - calibrate IMU using calculate_IMU_error() in the void setup() to get these values, then comment out calculate_IMU_error()
 float AccErrorX = 0.0;
-float AccErrorY = 0.0;
-float AccErrorZ = 0.0;
-float GyroErrorX = 0.0;
-float GyroErrorY = 0.0;
-float GyroErrorZ = 0.0;
+float AccErrorY = -0.01;
+float AccErrorZ = 0.07;
+float GyroErrorX = 5.1;
+float GyroErrorY = -0.93;
+float GyroErrorZ = 0.19;
 
 // Controller parameters (take note of defaults before modifying!):
 float i_limit = 25.0;  // Integrator saturation level, mostly for safety (default 25.0)
@@ -199,6 +229,13 @@ float Ki_yaw = 0.05;    // Yaw I-gain
 float Kd_yaw = 0.00015; // Yaw D-gain (be careful when increasing too high, motors will begin to overheat!)
 
 //========================================================================================================================//
+
+// WINGEDQUAD SPECIFIC VARIABLES
+
+// TODO: Some variable here
+
+
+//========================================================================================================================//
 //                                                     DECLARE PINS                                                       //
 //========================================================================================================================//
 
@@ -206,33 +243,33 @@ float Kd_yaw = 0.00015; // Yaw D-gain (be careful when increasing too high, moto
 // Radio:
 // Note: If using SBUS, connect to pin 21 (RX5), if using DSM, connect to pin 15 (RX3)
 #if not defined USE_CRSF_RX
-const int ch1Pin = 15; // throttle
-const int ch2Pin = 16; // ail
-const int ch3Pin = 17; // ele
-const int ch4Pin = 20; // rudd
-const int ch5Pin = 21; // gear (throttle cut)
-const int ch6Pin = 22; // aux1 (free aux channel)
-const int PPM_Pin = 23;
+const int ch1Pin = 0; // throttle
+const int ch2Pin = 0; // ail
+const int ch3Pin = 0; // ele
+const int ch4Pin = 0; // rudd
+const int ch5Pin = 0; // gear (throttle cut)
+const int ch6Pin = 0; // aux1 (free aux channel)
+const int PPM_Pin = 0;
 #endif
 
 // OneShot125 ESC pin outputs:
-const int m1Pin = 0;
-const int m2Pin = 1;
-const int m3Pin = 2;
+const int m1Pin = 23;
+const int m2Pin = 22;
+const int m3Pin = 4;
 const int m4Pin = 3;
 
 // PWM servo or ESC outputs:
-const int servo1Pin = 6;
-const int servo2Pin = 7;
-// const int servo3Pin = 8;
-// const int servo4Pin = 9;
-// const int servo5Pin = 10;
-// const int servo6Pin = 11;
-// const int servo7Pin = 12;
+const int servo1Pin = 10; // Wing rotating
+const int servo2Pin = 9;  // FPV camera angle changing
+const int servo3Pin = 6;  // Observer gimbal pan-axis
+const int servo4Pin = 5;  // Observer gimbal tilt_axis
+// const int servo5Pin = 0;
+// const int servo6Pin = 0;
+// const int servo7Pin = 0;
 PWMServo servo1; // Create servo objects to control a servo or ESC with PWM
 PWMServo servo2;
-// PWMServo servo3;
-// PWMServo servo4;
+PWMServo servo3;
+PWMServo servo4;
 // PWMServo servo5;
 // PWMServo servo6;
 // PWMServo servo7;
@@ -628,7 +665,7 @@ void loopBlink()
     else if (blinkAlternate == 0)
     {
       blinkAlternate = 1;
-      blink_delay = 2000000;
+      blink_delay = 750000;
     }
   }
 }
@@ -1628,8 +1665,8 @@ void calibrateESCs()
 
     servo1.write(s1_command_PWM);
     servo2.write(s2_command_PWM);
-    // servo3.write(s3_command_PWM);
-    // servo4.write(s4_command_PWM);
+    servo3.write(s3_command_PWM);
+    servo4.write(s4_command_PWM);
     // servo5.write(s5_command_PWM);
     // servo6.write(s6_command_PWM);
     // servo7.write(s7_command_PWM);
@@ -1802,25 +1839,24 @@ void printRadioData()
   if (current_time - print_counter > 10000)
   {
     print_counter = micros();
-    Serial.print(F(" CH1:"));
-    Serial.print(channel_1_pwm);
-    Serial.print(F(" CH2:"));
-    Serial.print(channel_2_pwm);
-    Serial.print(F(" CH3:"));
-    Serial.print(channel_3_pwm);
-    Serial.print(F(" CH4:"));
-    Serial.print(channel_4_pwm);
-    Serial.print(F(" CH5:"));
-    Serial.print(channel_5_pwm);
-    Serial.print(F(" CH6:"));
-    Serial.print(channel_6_pwm);
+    Serial.print(F(">CH1:"));
+    Serial.println(channel_1_pwm);
+    Serial.print(F(">CH2:"));
+    Serial.println(channel_2_pwm);
+    Serial.print(F(">CH3:"));
+    Serial.println(channel_3_pwm);
+    Serial.print(F(">CH4:"));
+    Serial.println(channel_4_pwm);
+    Serial.print(F(">CH5:"));
+    Serial.println(channel_5_pwm);
+    Serial.print(F(">CH6:"));
+    Serial.println(channel_6_pwm);
 #if defined USE_CRSF_RX
-    Serial.print(F(" CH7:"));
-    Serial.print(channel_7_pwm);
-    Serial.print(F(" CH8:"));
-    Serial.print(channel_8_pwm);
+    Serial.print(F(">CH7:"));
+    Serial.println(channel_7_pwm);
+    Serial.print(F(">CH8:"));
+    Serial.println(channel_8_pwm);
 #endif
-    Serial.println();
   }
 }
 
@@ -1829,13 +1865,13 @@ void printDesiredState()
   if (current_time - print_counter > 10000)
   {
     print_counter = micros();
-    Serial.print(F("thro_des:"));
-    Serial.print(thro_des);
-    Serial.print(F(" roll_des:"));
-    Serial.print(roll_des);
-    Serial.print(F(" pitch_des:"));
-    Serial.print(pitch_des);
-    Serial.print(F(" yaw_des:"));
+    Serial.print(F(">thro_des:"));
+    Serial.println(thro_des);
+    Serial.print(F(">roll_des:"));
+    Serial.println(roll_des);
+    Serial.print(F(">pitch_des:"));
+    Serial.println(pitch_des);
+    Serial.print(F(">yaw_des:"));
     Serial.println(yaw_des);
   }
 }
@@ -1845,11 +1881,11 @@ void printGyroData()
   if (current_time - print_counter > 10000)
   {
     print_counter = micros();
-    Serial.print(F("GyroX:"));
-    Serial.print(GyroX);
-    Serial.print(F(" GyroY:"));
-    Serial.print(GyroY);
-    Serial.print(F(" GyroZ:"));
+    Serial.print(F(">GyroX:"));
+    Serial.println(GyroX);
+    Serial.print(F(">GyroY:"));
+    Serial.println(GyroY);
+    Serial.print(F(">GyroZ:"));
     Serial.println(GyroZ);
   }
 }
@@ -1859,11 +1895,11 @@ void printAccelData()
   if (current_time - print_counter > 10000)
   {
     print_counter = micros();
-    Serial.print(F("AccX:"));
-    Serial.print(AccX);
-    Serial.print(F(" AccY:"));
-    Serial.print(AccY);
-    Serial.print(F(" AccZ:"));
+    Serial.print(F(">AccX:"));
+    Serial.println(AccX);
+    Serial.print(F(">AccY:"));
+    Serial.println(AccY);
+    Serial.print(F(">AccZ:"));
     Serial.println(AccZ);
   }
 }
@@ -1873,11 +1909,11 @@ void printMagData()
   if (current_time - print_counter > 10000)
   {
     print_counter = micros();
-    Serial.print(F("MagX:"));
-    Serial.print(MagX);
-    Serial.print(F(" MagY:"));
-    Serial.print(MagY);
-    Serial.print(F(" MagZ:"));
+    Serial.print(F(">MagX:"));
+    Serial.println(MagX);
+    Serial.print(F(">MagY:"));
+    Serial.println(MagY);
+    Serial.print(F(">MagZ:"));
     Serial.println(MagZ);
   }
 }
@@ -1887,11 +1923,11 @@ void printRollPitchYaw()
   if (current_time - print_counter > 10000)
   {
     print_counter = micros();
-    Serial.print(F("roll:"));
-    Serial.print(roll_IMU);
-    Serial.print(F(" pitch:"));
-    Serial.print(pitch_IMU);
-    Serial.print(F(" yaw:"));
+    Serial.print(F(">roll:"));
+    Serial.println(roll_IMU);
+    Serial.print(F(">pitch:"));
+    Serial.println(pitch_IMU);
+    Serial.print(F(">yaw:"));
     Serial.println(yaw_IMU);
   }
 }
@@ -1901,11 +1937,11 @@ void printPIDoutput()
   if (current_time - print_counter > 10000)
   {
     print_counter = micros();
-    Serial.print(F("roll_PID:"));
-    Serial.print(roll_PID);
-    Serial.print(F(" pitch_PID:"));
-    Serial.print(pitch_PID);
-    Serial.print(F(" yaw_PID:"));
+    Serial.print(F(">roll_PID:"));
+    Serial.println(roll_PID);
+    Serial.print(F(">pitch_PID:"));
+    Serial.println(pitch_PID);
+    Serial.print(F(">yaw_PID:"));
     Serial.println(yaw_PID);
   }
 }
@@ -1915,14 +1951,14 @@ void printMotorCommands()
   if (current_time - print_counter > 10000)
   {
     print_counter = micros();
-    Serial.print(F("m1_command:"));
-    Serial.print(m1_command_PWM);
-    Serial.print(F(" m2_command:"));
-    Serial.print(m2_command_PWM);
-    Serial.print(F(" m3_command:"));
-    Serial.print(m3_command_PWM);
-    Serial.print(F(" m4_command:"));
-    Serial.print(m4_command_PWM);
+    Serial.print(F(">m1_command:"));
+    Serial.println(m1_command_PWM);
+    Serial.print(F(">m2_command:"));
+    Serial.println(m2_command_PWM);
+    Serial.print(F(">m3_command:"));
+    Serial.println(m3_command_PWM);
+    Serial.print(F(">m4_command:"));
+    Serial.println(m4_command_PWM);
   }
 }
 
@@ -1931,19 +1967,19 @@ void printServoCommands()
   if (current_time - print_counter > 10000)
   {
     print_counter = micros();
-    Serial.print(F("s1_command:"));
-    Serial.print(s1_command_PWM);
-    Serial.print(F(" s2_command:"));
-    Serial.print(s2_command_PWM);
-    Serial.print(F(" s3_command:"));
-    Serial.print(s3_command_PWM);
-    Serial.print(F(" s4_command:"));
-    Serial.print(s4_command_PWM);
-    Serial.print(F(" s5_command:"));
-    Serial.print(s5_command_PWM);
-    Serial.print(F(" s6_command:"));
-    Serial.print(s6_command_PWM);
-    Serial.print(F(" s7_command:"));
+    Serial.print(F(">s1_command:"));
+    Serial.println(s1_command_PWM);
+    Serial.print(F(">s2_command:"));
+    Serial.println(s2_command_PWM);
+    Serial.print(F(">s3_command:"));
+    Serial.println(s3_command_PWM);
+    Serial.print(F(">s4_command:"));
+    Serial.println(s4_command_PWM);
+    Serial.print(F(">s5_command:"));
+    Serial.println(s5_command_PWM);
+    Serial.print(F(">s6_command:"));
+    Serial.println(s6_command_PWM);
+    Serial.print(F(">s7_command:"));
     Serial.println(s7_command_PWM);
   }
 }
@@ -1953,7 +1989,7 @@ void printLoopRate()
   if (current_time - print_counter > 10000)
   {
     print_counter = micros();
-    Serial.print(F("dt:"));
+    Serial.print(F(">dt:"));
     Serial.println(dt * 1000000.0);
   }
 }
@@ -1964,7 +2000,8 @@ void printLoopRate()
 
 void setup()
 {
-  Serial.begin(500000); // USB serial
+  Serial.begin(576000); // USB serial
+  while (!Serial) {} // Making sure the serial monitor is connected while debugging
   delay(500);
 
   // Initialize all pins
@@ -1975,8 +2012,8 @@ void setup()
   pinMode(m4Pin, OUTPUT);
   servo1.attach(servo1Pin, 900, 2100); // Pin, min PWM value, max PWM value
   servo2.attach(servo2Pin, 900, 2100);
-  // servo3.attach(servo3Pin, 900, 2100);
-  // servo4.attach(servo4Pin, 900, 2100);
+  servo3.attach(servo3Pin, 900, 2100);
+  servo4.attach(servo4Pin, 900, 2100);
   // servo5.attach(servo5Pin, 900, 2100);
   // servo6.attach(servo6Pin, 900, 2100);
   // servo7.attach(servo7Pin, 900, 2100);
@@ -2003,16 +2040,16 @@ void setup()
   delay(5);
 
   // Get IMU error to zero accelerometer and gyro readings, assuming vehicle is level when powered up
-  // calculate_IMU_error(); //Calibration parameters printed to serial monitor. Paste these in the user specified variables section, then comment this out forever.
+  // calculate_IMU_error(); // Calibration parameters printed to serial monitor. Paste these in the user specified variables section, then comment this out forever.
 
-  // Arm servo channels TODO: Determine and set the default angle
-  servo1.write(0); // Command servo angle from 0-180 degrees (1000 to 2000 PWM)
-  servo2.write(0); // Set these to 90 for servos if you do not want them to briefly max out on startup
-  // servo3.write(0); //Keep these at 0 if you are using servo outputs for motors
-  // servo4.write(0);
-  // servo5.write(0);
-  // servo6.write(0);
-  // servo7.write(0);
+  // Arm servo channels
+  servo1.write(servo1Default); // Command servo angle from 0-180 degrees (1000 to 2000 PWM)
+  servo2.write(servo2Default); // Set these to 90 for servos if you do not want them to briefly max out on startup
+  servo3.write(servo3Default); // Keep these at 0 if you are using servo outputs for motors
+  servo4.write(servo4Default);
+  // servo5.write(servo5Default);
+  // servo6.write(servo6Default);
+  // servo7.write(servo7Default);
 
   delay(5);
 
@@ -2084,8 +2121,8 @@ void loop()
   commandMotors();              // Sends command pulses to each motor pin using OneShot125 protocol
   servo1.write(s1_command_PWM); // Writes PWM value to servo object
   servo2.write(s2_command_PWM);
-  // servo3.write(s3_command_PWM);
-  // servo4.write(s4_command_PWM);
+  servo3.write(s3_command_PWM);
+  servo4.write(s4_command_PWM);
   // servo5.write(s5_command_PWM);
   // servo6.write(s6_command_PWM);
   // servo7.write(s7_command_PWM);
